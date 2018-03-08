4a6ca2386e74271b4ec61bec07ffe401bca83d9c
mochaul <praditomo@gmail.com>
2018-03-08 17:45:52 +0700

    fix factory pattern 2

--- tutorial-4/src/main/java/id/ac/ui/cs/advprog/tutorial4/exercise1/factory/DepokPizzaIngredientFactory.java
+++ tutorial-4/src/main/java/id/ac/ui/cs/advprog/tutorial4/exercise1/factory/DepokPizzaIngredientFactory.java
@@ -1,17 +1,17 @@
 package id.ac.ui.cs.advprog.tutorial4.exercise1.factory;
 
 import id.ac.ui.cs.advprog.tutorial4.exercise1.factory.cheese.Cheese;
-import id.ac.ui.cs.advprog.tutorial4.exercise1.factory.cheese.MimiCheese;
+import id.ac.ui.cs.advprog.tutorial4.exercise1.factory.cheese.CheddarCheese;
 import id.ac.ui.cs.advprog.tutorial4.exercise1.factory.clam.Clams;
-import id.ac.ui.cs.advprog.tutorial4.exercise1.factory.clam.SmokedClams;
+import id.ac.ui.cs.advprog.tutorial4.exercise1.factory.clam.SteamedClams;
 import id.ac.ui.cs.advprog.tutorial4.exercise1.factory.dough.Dough;
-import id.ac.ui.cs.advprog.tutorial4.exercise1.factory.dough.ThiccCrustDough;
-import id.ac.ui.cs.advprog.tutorial4.exercise1.factory.sauce.MicinSauce;
+import id.ac.ui.cs.advprog.tutorial4.exercise1.factory.dough.MediumCrustDough;
+import id.ac.ui.cs.advprog.tutorial4.exercise1.factory.sauce.SambaladoSauce;
 import id.ac.ui.cs.advprog.tutorial4.exercise1.factory.sauce.Sauce;
 import id.ac.ui.cs.advprog.tutorial4.exercise1.factory.veggies.BlackOlives;
 import id.ac.ui.cs.advprog.tutorial4.exercise1.factory.veggies.Mushroom;
 import id.ac.ui.cs.advprog.tutorial4.exercise1.factory.veggies.RedPepper;
-import id.ac.ui.cs.advprog.tutorial4.exercise1.factory.veggies.Salak;
+import id.ac.ui.cs.advprog.tutorial4.exercise1.factory.veggies.Kangkung;
 import id.ac.ui.cs.advprog.tutorial4.exercise1.factory.veggies.Veggies;
 
 public class DepokPizzaIngredientFactory implements PizzaIngredientFactory {

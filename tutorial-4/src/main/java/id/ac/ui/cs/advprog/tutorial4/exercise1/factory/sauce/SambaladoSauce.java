b607a15e6988fc58549467b7dd3d630c5dba4415
mochaul <praditomo@gmail.com>
2018-03-08 17:38:09 +0700

    finish factory pattern

---
+++ tutorial-4/src/main/java/id/ac/ui/cs/advprog/tutorial4/exercise1/factory/sauce/SambaladoSauce.java
@@ -0,0 +1,7 @@
+package id.ac.ui.cs.advprog.tutorial4.exercise1.factory.sauce;
+
+public class SambaladoSauce implements Sauce {
+    public String toString() {
+        return "Sambalado sauce";
+    }
+}
---
+++ tutorial-4/src/main/java/id/ac/ui/cs/advprog/tutorial4/exercise1/factory/veggies/Kangkung.java
@@ -0,0 +1,8 @@
+package id.ac.ui.cs.advprog.tutorial4.exercise1.factory.veggies;
+
+public class Kangkung implements Veggies {
+
+    public String toString() {
+        return "Kangkung-kangkung";
+    }
+}

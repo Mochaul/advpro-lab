d86947ef98a10fb314f95eb2eeaeb6d41b54b45f
Mochamad Aulia <praditomo@gmail.com>
2018-03-23 20:11:31 +0700

    finish rental

--- tutorial-6/src/main/java/Rental.java
+++ tutorial-6/src/main/java/Rental.java
@@ -15,4 +15,33 @@ class Rental {
     public int getDaysRented() {
         return daysRented;
     }
+
+    public double getPriceAmount() {
+        // Determine amount for each line
+        double thisAmount = 0.0;
+        switch (movie.getPriceCode()) {
+            case Movie.REGULAR:
+                thisAmount += 2;
+                if (daysRented > 2)
+                    thisAmount += (daysRented - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                thisAmount += daysRented * 3;
+                break;
+            case Movie.CHILDREN:
+                thisAmount += 1.5;
+                if (daysRented > 3)
+                    thisAmount += (daysRented - 3) * 1.5;
+                break;
+        }
+        return thisAmount;
+    }
+
+    public int getFrequentRenterPoints() {
+        // Add bonus for a 2 day new release rental
+        if ((movie.getPriceCode() == Movie.NEW_RELEASE) &&
+                daysRented > 1)
+            return 2;
+        return 1;
+    }
 }

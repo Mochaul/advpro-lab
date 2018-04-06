badb6466c61f00d80a95bd1338a623da616ea7f2
mochaul <praditomo@gmail.com>
2018-04-06 21:12:57 +0700

    finish 1st exercise

--- tutorial-7/src/main/java/PrimeChecker.java
+++ tutorial-7/src/main/java/PrimeChecker.java
@@ -11,7 +11,7 @@ public class PrimeChecker {
 
     public static void main(String[] args) {
         for (int i = 1; i < 10; i++) {
-            System.out.println(String.format("isPrime(%d)? %b", i, isPrime(i)));
+            System.out.println(String.format("isPrime(%d)? %b", i, isPrime(i))); 
         }
     }
 }
